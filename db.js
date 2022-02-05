53cdbc38794bb64c4ca07e2239c42aff402bcdc5
doruksahin <doruk.sahin@inavitas.com>
2022-02-05 13:55:29 +0300

    adds postgresql configs

--- db.js
+++ db.js
@@ -1,15 +1,28 @@
 const mongoose = require("mongoose");
+const { Client } = require('pg');
 
 module.exports = async () => {
-    try {
-        const connectionParams = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        };
-        await mongoose.connect(global.DB_STRING, connectionParams);
-        console.log("connected to database.");
-    } catch (error) {
-        console.log("could not connect to database", error);
+    if (global.DB_TYPE === 'mongodb') {
+        try {
+            const connectionParams = {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+            };
+            await mongoose.connect(global.MONGO_DB_STRING, connectionParams);
+            console.log("connected to database.");
+        } catch (error) {
+            console.log("could not connect to database", error);
+        }
+    } else if (global.DB_TYPE === 'postgresql') {
+        global.client = new Client({
+            user: global.PQ_USERNAME,
+            host: global.PQ_HOSTNAME,
+            database: global.PQ_DATABASE,
+            password: global.PQ_PASSWORD,
+            port: global.PQ_PORT,
+        });
     }
+
+
 };
