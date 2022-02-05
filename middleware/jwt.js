36daef41239e9596e4efb14e960f619570e9c537
doruksahin <doruk.sahin@inavitas.com>
2022-02-05 13:18:41 +0300

    .

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,21 +1,21 @@
 const jwt = require("jsonwebtoken")
 const { User, validateUser } = require("../models/user")
 
-function ensureToken(req, res, next){
+function ensureToken(req, res, next) {
     const bearerHeader = req.headers["authorization"]
-    if(typeof bearerHeader !== 'undefined'){
+    if (typeof bearerHeader !== 'undefined') {
         const token = bearerHeader.split(" ")[1]
 
-        jwt.verify(token, process.env.JWTPRIVATEKEY, async function(err, data){
-            if(err){
+        jwt.verify(token, global.JWTPRIVATEKEY, async function (err, data) {
+            if (err) {
                 res.status(403).json(err)
-            }else{
-                req.myUser = await User.findOne({ _id: data._id});
-                if(!req.myUser) res.status(403).send("user not found (refresh jwt)")
+            } else {
+                req.myUser = await User.findOne({ _id: data._id });
+                if (!req.myUser) res.status(403).send("user not found (refresh jwt)")
                 next()
             }
         })
-    }else{ 
+    } else {
         res.status(403).send("JWT is null")
     }
 }
